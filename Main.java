5142b5b3713540e941b65318ebdcb64a4aad0833
haripri0109r <haripri0109r@gmail.com>
2025-09-14 21:10:36 +0530

    task

--- Main.java
+++ Main.java
@@ -2,20 +2,28 @@ import java.util.Scanner;
  
 
 class Task {
-    private int id;
+     private int id;
     private String title;
+    private String priority;
+    private boolean isCompleted;
 
-    public Task(int id, String title) {
+    public Task(int id, String title, String priority) {
         this.id = id;
         this.title = title;
+        this.priority = priority;
+        this.isCompleted = false;
     }
 
     public int getId() { return id; }
     public String getTitle() { return title; }
+    public String getPriority() { return priority; }
+    public boolean isCompleted() { return isCompleted; }
+
+    public void setCompleted(boolean completed) { this.isCompleted = completed; }
 
     @Override
     public String toString() {
-        return id + " | " + title;
+        return id + " | " + title + " | Priority: " + priority + " | Status: " + (isCompleted ? "Completed" : "Pending");
     }
 }
 
