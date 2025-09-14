4972e57f0f4013ae4981bd112a8c0ba6432de914
haripri0109r <haripri0109r@gmail.com>
2025-09-14 20:56:57 +0530

    Menu

--- Main.java
+++ Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Smart Time!");
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("\n--- Smart Time ---");
+            System.out.println("1. Add Task");
+            System.out.println("2. View Tasks");
+            System.out.println("3. Exit");
+            System.out.print("Enter choice: ");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1 -> addTask();
+                case 2 -> viewTasks();
+                case 3 -> System.out.println("Exiting...");
+                default -> System.out.println("Invalid choice!");
+            }
+        } while (choice != 3);
+        sc.close();
+    }
+
+    private static void addTask() {
+        // to be implemented
+    }
+
+    private static void viewTasks() {
+        // to be implemented
     }
 }
